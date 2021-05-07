a1267c19684e176f6b220d0d03ab7527aebfba46
StrajnarFilip <46705237+StrajnarFilip@users.noreply.github.com>
2021-05-07 14:47:37 +0200

    Add files via upload

---
+++ miner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd
+mkdir mining
+cd mining/
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-focal-x64.tar.gz
+tar xf xmrig-6.12.1-focal-x64.tar.gz
+cd xmrig-6.12.1/
+rm config.json
+wget https://raw.githubusercontent.com/StrajnarFilip/MoneroMiner/main/config.json -O config.json
+./xmrig
