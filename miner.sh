4d1d6f2141b55ad4629ff6022ab9a86a7b9955cd
g00sifer <87793505+g00sifer@users.noreply.github.com>
2021-07-22 02:26:16 -0400

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -6,5 +6,5 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-focal
 tar xf xmrig-6.12.1-focal-x64.tar.gz
 cd xmrig-6.12.1/
 rm config.json
-wget https://raw.githubusercontent.com/g00sifer/MoneroMiner/main/config.json -O config.json
+wget https://raw.githubusercontent.com/g00sifer/MoneroMiner/blob/main/config.json -O config.json
 ./xmrig
